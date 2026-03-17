047a9f8bc4500f5c5853acc2fe0ad1d762cb6868
Shubham yadav <shubhamyadav1662000@gmail.com>
2026-03-17 14:10:48 +0530

    Layout and multiple routing set using react route dom and Navlik, Link

---
+++ pages/Navbar.jsx
@@ -0,0 +1,9 @@
+import { eact } from "react"
+const Navbar = () => {
+    return (
+        <>
+            <h1 className="text-black text-3xl mt-20 bg-amber-300">This is navbar </h1>
+        </>
+    )
+}
+export default Navbar
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,28 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from '../components/home/Home.jsx'
+import About from '../components/about/About.jsx'
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <Layout />,
+  children: [{
+    path: '',
+    element: <Home />
+  },
+  {
+    path: "about",
+    element: <About />
+  },
+  {}
+  ]
+}])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
